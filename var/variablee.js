839b57e459767f6366ab4ee74d72806faa3ab62e
Fizza-lab <h48993832@gmail.com>
2025-12-24 17:20:34 +0000

    container dev confg

---
+++ var/variablee.js
@@ -0,0 +1,14 @@
+let accountId=123456
+let accountEmail="hadi786@gmail.com"
+let accountPassword="hadi@786"
+let accountCity="MDK"
+let accountState
+accountId=786543
+accountEmail="Fiza@gmail.com"
+accountPassword="haider@123"
+accountCity="Lahore"
+
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState]);
+/*
+perfer not to use var because issue in block scope and functional scope.
+*/
