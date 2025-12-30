9218fb61da915cade367e6a8757213dc6e44ae36
Fizza-lab <h48993832@gmail.com>
2025-12-30 07:37:28 +0000

    coversion.js

---
+++ var/coversion.js
@@ -0,0 +1,20 @@
+let score="33abcd"
+//console.log(typeof score);
+//console.log(score);
+let ValueInNumber=Number(score)
+//console.log( typeof ValueInNumber);
+//console.log( ValueInNumber);
+let anynumber=1
+let booleannumber= Boolean(anynumber)
+//console.log(booleannumber);
+let somenumber=100
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+
+// asi me ham ne number ko string me or string ko number me or pr bolan ko null undefied sb ki conversion ki h.
+
+
+
+
+
+
