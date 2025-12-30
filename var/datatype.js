2f7e3121cca00be6bfd72005491a3082bbe4e81c
Fizza-lab <h48993832@gmail.com>
2025-12-30 07:20:14 +0000

    datatype.js var

--- var/datatype.js
+++ var/datatype.js
@@ -1,2 +1,22 @@
 "use strict"; // treat all js code as newer version.
-// alert(3+3)// we are using nodejs, not browser
+// alert(3+3)// we are using nodejs, not browser.
+let name ="Fizza"
+let age=23
+let logged=true
+let state;
+let something=undefined
+let anynumber=null
+console.log(typeof "name");
+console.log(typeof age);
+console.log(typeof logged);
+console.log(typeof state);
+console.log(typeof(something));
+console.log(typeof(anynumber));
+// Notes
+// Number=> k lye use kiys jta  h ye bi data type h ais me ham 2 to power 53 he rkhty h agr ham ne lmba number lekhna ho to pr ham na bigint use krty ha.
+// string =>k lye ham double qoute me lekhty hn ya signle qoute me lekhty hain.
+//  symbole=> ye unqiness k lye use hota h ye zda tar ham react me use krty hai.
+//undefined=> ye ham tb use kety h jb variable declare to kr dy bs value na dy.
+// null=> ye bi ak trh ki data type he h lkn ham aisy specific value k lye us krty hain.
+// object ham next detail se parhy gy object k bry me.
+// boolean=> ais me ham na ture or false me bs value dety milti hain.
