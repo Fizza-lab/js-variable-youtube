5c11377fd8cf7fdfdb059dd9fbca4c2b2d21f3aa
Fizza-lab <h48993832@gmail.com>
2026-01-01 08:55:42 +0000

    datatype summary.js

---
+++ var/datatype summary.js
@@ -0,0 +1,14 @@
+// Notes 
+// datatypes types?
+// two types of datatypes.
+1.// Primitive datatype
+2.// Non-Primitive datatypes.
+// Primitive datatypes: ye ak datatype h ais me ham na sirf or simple vlue stored krwty hain.they can hold only one value at a time and are not a object.
+//Primitive datatypes:
+// 7 primitive datatype : num, string,bigint,boolean,symbol,null, undefined.
+let numvalue=34
+let nameemploye="Fizza"
+let isloggedIn=false
+let state;
+let storetemp=null
+let 
