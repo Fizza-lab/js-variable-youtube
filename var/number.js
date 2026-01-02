a632590be5cdcf5215ba8a8ef3a04d65e67b5522
Fizza-lab <h48993832@gmail.com>
2026-01-02 21:18:54 +0000

    number.js

---
+++ var/number.js
@@ -0,0 +1,49 @@
+// let score=400
+// let balnace=new Number(300)
+// console.log(score);
+// console.log(balnace);
+// console.log(  balnace.toString().length);
+// console.log(balnace.toFixed(1));
+// const anotherNumber=124.899
+// console.log(anotherNumber.toPrecision(4)); 
+// let hunreds=10000000
+// console.log(hunreds.toLocaleString('en-PK'));
+// let score=400
+// console.log(score);
+// let balance=new Number(750)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// let num=456.789
+// console.log(num.toFixed(2));
+// let num1=3.14159265
+// console.log(num1.toPrecision(5));
+// let num3=8500000
+// console.log(num3.toLocaleString('en-Pk'));
+// **** Math *****/
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(8.9));
+// console.log(Math.floor(7.1));
+// console.log(Math.sqrt(100));
+// console.log(Math.min(5,9,0,1,3,4));
+// console.log(Math.max(4,40,80,87,90,32));
+// console.log(Math.random());//ais ki value hmesh 0 or 1 me ati h.
+// console.log((Math.random()*20)+1);
+// const min=20
+// const max=10
+// console.log(Math.floor(Math.random()*(max-min+1 ))+min);
+// console.log(Math.abs(-15));
+// console.log(Math.round(7.8));
+// console.log(Math.ceil(9.3));
+// console.log(Math.floor(6.7));
+// console.log(Math.sqrt(121));
+// console.log(Math.min(12,5,8,20,3));
+// console.log(Math.max(12,5,8,13));
+// console.log(Math.random);
+// console.log(Math.random());
+// console.log((Math.random()*30)+1);
+// let max=20
+// let min=10
+// console.log( Math.floor(Math.random()*(max-min+1))+min);
