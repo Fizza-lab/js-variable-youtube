e6846d4cc6ccb2b5d174695761c2cddb38a1c82d
Fizza-lab <h48993832@gmail.com>
2026-01-02 16:46:14 +0000

    stack.js

---
+++ var/stack.js
@@ -0,0 +1,22 @@
+// memory two tarha ki h 
+1.//ak stack memeory ha or 2. heap memory 
+// dono kah kah use hoti h
+// stack memory jo h wo na primitive data type me use hoti h.
+// or jo ha heap wo non primitive ya reference datatype me use hoti hai.
+// jb bi memory stack me koi variable decalre hota h to oas ki copy hmy militi hai.
+// lkn jb memory heap me koi variable declare krty h to wo reference me se lty hain
+let myYoutubename="Fizaashahazdidotcom  "
+let anothername=myYoutubename
+console.log(anothername);
+anothername="FyizzaYoutubeChannel"
+console.log(myYoutubename);
+console.log(anothername);
+let userOne={
+    name:'Fizza',
+    age:34,
+    email:"hadiabdual@gmail.com"
+}
+let userTwo= userOne;
+userTwo.age=54
+console.log(userOne.age);
+console.log(userTwo.age);
