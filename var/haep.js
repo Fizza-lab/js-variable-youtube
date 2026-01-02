31aca9d915c11dc831d2c81bd70b843efae3dea0
Fizza-lab <h48993832@gmail.com>
2026-01-02 16:56:15 +0000

    haep.js

---
+++ var/haep.js
@@ -0,0 +1,15 @@
+let mychannel="Bilalhaiderdotcom"
+let anotherchannel=mychannel
+anotherchannel="Fizzadotcom"
+console.log(mychannel);
+console.log(anotherchannel);
+let userone={
+  name:'Bilal',
+  age:22,
+  address:"MDK",
+  email:'bilal@gmail.com',
+}
+let usertwo=userone
+usertwo.address='Lhr'
+console.log(userone.address);
+console.log(usertwo.address);
