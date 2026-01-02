5937267fc9eb890ae90afe014edd1af14d1dab8f
Fizza-lab <h48993832@gmail.com>
2026-01-02 07:44:03 +0000

    test.js

--- var/datatype summary.js
+++ var/datatype summary.js
@@ -6,9 +6,30 @@
 // Primitive datatypes: ye ak datatype h ais me ham na sirf or simple vlue stored krwty hain.they can hold only one value at a time and are not a object.
 //Primitive datatypes:
 // 7 primitive datatype : num, string,bigint,boolean,symbol,null, undefined.
-let numvalue=34
-let nameemploye="Fizza"
-let isloggedIn=false
-let state;
-let storetemp=null
-let 
+// let numvalue=34
+// let nameemploye="Fizza"
+// let isloggedIn=false
+// let state;
+// let storetemp=null
+// const Id= Symbol("123")
+// const anotherId= Symbol("123")
+//console.log(numvalue,nameemploye,isloggedIn,state,storetemp);
+// console.log(Id === anotherId);
+// const bigintNum=23456789876n
+// console.log(bigintNum);
+// const Id=Symbol('123')
+//  const anotherId=Symbol('123')
+// console.log(Id===anotherId);
+// const bigintNum=23456789321n
+// console.log(bigintNum);
+// Non Primivtie datatype: ye  bi datatype h ais ham multiple value or complex data store krwny k lye use krty hain.they stored by reference and can be changed. ais ko ham reference data type bi khty hain
+// Non primitive datatype ki types:array, Object,Function.
+const city=["Pakistan","Lahore","MDk" , "Multan"];
+let myobj={
+    name:"Fizza",
+    age:23,
+    Email:"hadiabdual786@gmail.com"
+}
+const myFunction=function(){
+ console.log("Hello World!");
+}
---
+++ var/test.js
@@ -0,0 +1,22 @@
+// Primitive datatype
+let numvalue=34
+ let nameemploye="Fizza"
+ let isloggedIn=false 
+ let state;
+let storetemp=null
+console.log(numvalue,nameemploye,isloggedIn,state,storetemp);
+const Id=Symbol('123')
+ const anotherId=Symbol('123')
+console.log(Id===anotherId);
+const bigintNum=23456789321n
+console.log(bigintNum);
+// Non Primitive datatype
+const city=["Pakistan","Lahore","MDk" , "Multan"];
+let myobj={
+    name:"Fizza",
+    age:23,
+    Email:"hadiabdual786@gmail.com"
+}
+const myFunction=function(){
+ console.log("Hello World!");
+}
