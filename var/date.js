c80a0ac3ebf32593bd1494a4be5c1d7a5d94d88a
Fizza-lab <h48993832@gmail.com>
2026-01-03 05:43:03 +0000

    data.js

---
+++ var/date.js
@@ -0,0 +1,35 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+// let myCreatedDate=new Date(2025,0,23);
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toJSON());
+// console.log(myCreatedDate.toLocaleDateString());
+// let mytimeStamp=Date.now()
+// console.log(mytimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+// let newDate=new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+// newDate.localString('dafault',{
+//     weekday:"long"
+// })
+/// Paractice///
+// let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+ let myCreatedDate=new Date(2023,2,10)
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+let mytimeStamp= Date.now()
+console.log(mytimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
