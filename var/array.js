89a574337d4b6c440db4ce88b5aa314ffe9489c0
Fizza-lab <h48993832@gmail.com>
2026-01-19 08:46:23 +0000

    array.js

---
+++ var/array.js
@@ -0,0 +1,13 @@
+const myArr=[0,1,2,3,4,5]  // array k andr jo h wo elements hain.
+//console.log(myArr);
+// Array Method
+// push method ais me ham array k last pr ak elemnt add knry k lye use krty hain.
+//console.log(myArr);
+//myArr.pop(6);
+//myArr.pop(7);
+//console.log(myArr);
+//console.log(myArr);
+myArr.unshift(10);
+//console.log(myArr);
+myArr.shift()
+//console.log(myArr);
