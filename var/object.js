97e4c8278e891a31d3c04c10de25a98c103d59e1
Fizza-lab <h48993832@gmail.com>
2026-01-21 17:30:36 +0000

    object.js

---
+++ var/object.js
@@ -0,0 +1,17 @@
+// singleton // construct sy hmesha singletone bnta h .
+// agr ham object ko literial ki trh bnty han to singletone ni bnta hai.
+// object ko 2 trha se define krty hn construct triky se or literals triky se
+// ham jb bi construct triky se bnty hn to wo na singletone me bnty han.
+Object.create // to ye construct wala trika hai.
+
+// object literals
+const jsUser = {
+ name: "fizza",
+ age:  24,
+email: "fizza786@gmail.com",
+location: "MDK",
+isloggedIn:false,
+lastlogindays: ["Monday","Tuesday","wednesday","Thursday","Friday","Saturday", "Sunday"],
+
+}   // ye lliterals method hai ye kribrackrt h ye object hai.
+
