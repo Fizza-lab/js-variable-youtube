2994a2cb88e47993159be857aca7ed41865ec23b
Fizza-lab <h48993832@gmail.com>
2026-01-21 06:12:23 +0000

    array.js

--- var/array.js
+++ var/array.js
@@ -1,13 +1,30 @@
-const myArr=[0,1,2,3,4,5]  // array k andr jo h wo elements hain.
+const myArr=[ 0,1,2,3,4,5]  // array k andr jo h wo elements hain.
 //console.log(myArr);
 // Array Method
 // push method ais me ham array k last pr ak elemnt add knry k lye use krty hain.
 //console.log(myArr);
-//myArr.pop(6);
+//myArr.pop(6);//last pr se element ko remove krty h pop me.
 //myArr.pop(7);
 //console.log(myArr);
 //console.log(myArr);
-myArr.unshift(10);
+myArr.unshift(10);// first pr element add krny k lye use kiya h.
 //console.log(myArr);
-myArr.shift()
+myArr.shift()// first pr se element remove krny k lye use kiya h 
 //console.log(myArr);
+// 2 3 method h array ko declaie krny k 
+const myHeros=["Fizza","shaktiman","naagraj"]
+const myArr2 = new Array(1,2,3,4,5,6,7,8,9,10)
+//console.log(myArr2.includes(5));
+//console.log(myHeros);
+//console.log(myArr.indexOf(10));
+const newArr = myArr.join()
+//console.log(myArr);
+//console.log(typeof newArr);
+//  slice method or splice method ye inteview question k hwaly se bi dekhty.
+console.log("A", myArr);
+const myn1= myArr.slice(1,3)
+console.log(myn1);
+console.log("B", myArr);
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("C", myArr);
