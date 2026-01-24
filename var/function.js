8e98d60084202393ea3cf051e85f730143b5e3ba
Fizza-lab <h48993832@gmail.com>
2026-01-24 06:17:42 +0000

    function.js

---
+++ var/function.js
@@ -0,0 +1,13 @@
+ function sayMyName(){
+     console.log("F")
+     console.log("i")
+     console.log("z")
+     console.log("z")
+     console.log("a")
+ }
+// sayMyName() // jb ap function ki defintion bnty hn to jo bi input ham lety hn unhy ham parameters bolty.or jb function ko jb cl kiya jta h or jb value pass krty hn unhy aruments bola jta hai.
+function addanyTwoNum(Num1,Num2){
+ console.log(Num1+Num2);
+}
+addanyTwoNum("3",8)
+ 
