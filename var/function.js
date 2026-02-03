8e0f5b884ed1caf752ebf28ab9f8bb0247bb3b87
Fizza-lab <h48993832@gmail.com>
2026-02-03 08:19:02 +0000

    function.js

--- var/function.js
+++ var/function.js
@@ -1,13 +1,147 @@
- function sayMyName(){
-     console.log("F")
-     console.log("i")
-     console.log("z")
-     console.log("z")
-     console.log("a")
- }
-// sayMyName() // jb ap function ki defintion bnty hn to jo bi input ham lety hn unhy ham parameters bolty.or jb function ko jb cl kiya jta h or jb value pass krty hn unhy aruments bola jta hai.
-function addanyTwoNum(Num1,Num2){
- console.log(Num1+Num2);
-}
-addanyTwoNum("3",8)
- 
+// // // //  function k mtlb h jo bi ham ne code lekha h 10 lines k 20 lines k unhy 1 pakge me bnda krna h oasy ham function khty h.
+// // // // console.log("f");
+// // // // console.log("i");
+// // // // console.log("z");
+// // // // console.log("z");
+// // // // console.log("a");
+// // // function sayMyName(){ // yah ham ne function k keyword liya fuction k name lekha h or braket k andr ham ne jo lekha h wo oas fuction ki defintion ha.
+// // // console.log("f");
+// // // console.log("i");
+// // // console.log("z");
+// // // console.log("z");
+// // // console.log("a");
+// // // }
+// // // //sayMyName() // agr ham kahli function k name lekhty hn to mtlb ham function k bs refernce dy ry or agr ham function k name k agy braket lgty hain to mtlb ham ne function ko execute krwna ha
+// // // // agr bs function k name lekhty hin bs ye bta ry hoty hn k hm function k bs refernce dy hn jb execute krwn ho tb hain brackt lgty hn.
+// // // function addTwoanyNumbers(number1,number2){
+// // //     console.log(number1+number2);
+// // // }
+// // // //addTwoanyNumbers(76,90)
+// // // //addTwoanyNumbers(3,"90") // jb ham fuction ki defintion bnty han to oasy k ander ham jo bi input pas krty hn to wo ham unko parameters bolty hain.or jb ham fuction ko cl krty hin to tb ham jo input dety hn wo argument khty hn oasy ham.
+// // // function addTwoanyNumbers(number1,number2){
+// // //     //let result= number1+number2
+// // //     // console.log("Result:",result);
+// // //     // return result
+// // //     // console.log("Result:",result);
+// // //     // //console.log("fizza");
+// // //     return number1 + number2
+// // // }
+
+// // // const result= addTwoanyNumbers(3,8)
+// // //  //console.log("Result:",result);
+// // //  function loginuserMessage(username="fiza"){
+// // //     if(!username){
+// // //         console.log("please enter a username")
+// // //         return
+// // //     }
+// // //     return `${username} just loggedin.` 
+// // //  }
+// // //  //const username= loginuserMessage("fizza")
+// // //   console.log(loginuserMessage("hadi")) ;
+// // //   function addTwoanyNumbers(num1, num2) {
+// // //   return num1 + num2;
+// // // }
+
+// // // console.log(addTwoanyNumbers(5, 7));
+// // // function loginuserMessage(username = "fiza") {
+// // //   if (!username) {
+// // //     return "please enter a username";
+// // //   }
+// // //   return `${username} just logged in`;
+// // // }
+
+// // // console.log(loginuserMessage(""));
+// // // function twonmbers(num1,num2){
+// // //   return num1+num2
+// // // }
+// // //    const result=twonmbers(3,6)
+// // // console.log("Result:",result)
+// // // function demo(x = 10) {
+// // //   return x;
+// // // }
+// // // console.log(demo());
+// // // const result = addTwoanyNumbers(3, 8);
+// // // console.log(result);
+// // // function loginuserMessage(username = "fiza") {
+// // //   if (!username) {
+// // //     console.log("please enter a username");
+// // //     return;
+// // //   }
+// // //   return `${username} just loggedin.`;
+// // // }
+// // // loginuserMessage("");
+// // // function calculatecardprice(...num1){///yah pr rest operator uses kiya h ta k ham multiple values ko arry me print krwa skhy
+// // // return num1
+// // // }
+// // // console.log(calculatecardprice(200,500,800,100,300));
+// // // function sayMyName(){
+// // //     console.log("f");
+// // //     console.log("i");
+// // //     console.log("z");
+// // //     console.log("z");
+// // //     console.log("a");
+// // // }
+// // // //sayMyName();
+// // // function addTwoanyNumbers(number1,number2){
+// // //     // console.log(number1+number2);
+// // //     // let result =number1+number2
+// // //     // return result
+// // //     // return number1+number2
+// // // }
+// // // const result=addTwoanyNumbers("9",9);
+// // // // console.log("Result:",result);
+// // // function userloggedin(username="fiza"){
+// // //     if(!username){
+// // //         console.log("please enter a username.")
+// // //         return
+// // //     }
+// // //     return `${username} just loggedin.`
+// // // }
+// // // // userloggedin();
+// // // // console.log(userloggedin("hadi"));
+// // function shoppingcardprice(var1,var2,var3,var5,...num1){ /// rest operator k mtlb h yah k bht sri values apko kholy me di h ais ko bundle me dy dy pack kr k.
+// // return num1
+// // }
+// // //console.log(shoppingcardprice(200,400,500,900,1000.100000,500,4000,100));
+// // // yah pr ham ne jo var use kiya h ais me ham jo console log me jo value dy ry bta ry h ye k phli value 1 me doseri var 2me or 3 var me store kr dy bki bd me ap muje jo value rest me oasy print krwa k dy dy.
+// // const user={
+// //    username:"fizza",
+// //    price:200,
+// // }
+// // function hundleobject(anyobject){
+// //  console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+// // }
+// // //hundleobject(user)
+// // hundleobject({
+// //   username: "sam",
+// //   price: 395,
+// // });
+// // const myNewArray=[200,300,400,500,600]
+// // function returnsecondvalue(getArray){
+// //    return getArray[4]
+
+// // }
+// // //console.log(returnsecondvalue(myNewArray));
+// // console.log(returnsecondvalue([200,300,400,500,600]));
+// function shoppingcardprice(num1,var2,...var1){// multiple values ko ham ak he block me lany k lye ham rest operator usekrty hain
+//      return var1
+// }
+// // console.log(shoppingcardprice(200,300,400,500,700,));
+// const objecthundle ={
+//    username:"fiza",
+//    price:900,
+// }
+// function object(anyobject){
+//    console.log(`My username is ${anyobject.username} and is price ${anyobject.price}`);
+// }
+// //object(objecthundle);
+
+// // object({
+// //   username: "hadi",
+// //   price: 999,
+// // });
+// const myNewArray =[200,300,400,500,600,700]
+// function returnsecondvalue(getArray){
+//    return getArray[3]
+// }
+// console.log(returnsecondvalue(myNewArray));
---
+++ var/scope.js
@@ -0,0 +1,21 @@
+// // scope ak bht bra topic hai {} ye scope h ais ko ham scope khty hn.
+// // jb ye brackt kese function k sth ati h ye if else k sth ati to ye oas k scope hota h yanni function k scope ya if else k scope etc.
+// let a= 10
+// const b=20
+// var c=30
+
+// if(ture){
+// let a = 10
+// const b = 20
+// var c = 30
+// }
+// // console.log(a);
+// // console.log(b);
+//  console.log(c);
+let a = 10
+const b = 20
+var c= 30
+console.log(a);
+console.log(b);
+console.log(c);
+{}// ais ko ham scope bolty hn or ye scope jo h wo na ham function me ye use krty h if else me bi use krty hain.
