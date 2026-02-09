da6643481137a7fafb6c308afed435772ae64583
Fizza-lab <h48993832@gmail.com>
2026-02-09 11:52:38 +0000

    let.js

---
+++ var/let.js
@@ -0,0 +1,15 @@
+//js me ye keyword k diffference he ye h  jasy he apko keyword mily to oasy memory me store kr to mmemory ko name dy do or asi ko value assign kr do.
+const accountId = 112344      // const me ham value ko chnge ni kr skhty ye ak modern way h lkn ais me ham value jb delcare krty h to ham oas value ko dobar se chnge ni kr skhty.ais lye jb ham ne dekhna ho k ham ne ab koi value chnge ni krni o ham pr tb jo h const use krty hn ais me ham na dobra oas name k value dcleare kr kshty hn or na he ham jo h oas ki value ko chnge kr skhty hain.
+let accountEmail="hadi786gmail.com"
+var accountpassword="12345",
+ accountCity="MDK"
+ let accountState;
+//  accountID=2 // not alllowed h kuk ham const me jo h wo sirf or sirf ak tb he value declare kr skhty dobra na variabe or na vlue dono ko ham chnge ni kr skhty.
+ console.log(accountId,accountEmail,accountCity,accountpassword);
+ accountEmail="fizza786gmail.com"
+ accountpassword="7869"
+ accountCity="Lahore"
+ console.table([accountId,accountEmail,accountpassword,accountCity,accountState]);
+ /*Perfer not to use var.becasue of issue in block scope and functional scope.*/
+
+
