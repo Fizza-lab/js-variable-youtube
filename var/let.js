d7d232ddb8b79961cec765ae8a4ce8d297fb3fe1
Fizza-lab <h48993832@gmail.com>
2026-02-11 10:39:45 +0000

    let.js

--- var/let.js
+++ var/let.js
@@ -5,11 +5,38 @@ var accountpassword="12345",
  accountCity="MDK"
  let accountState;
 //  accountID=2 // not alllowed h kuk ham const me jo h wo sirf or sirf ak tb he value declare kr skhty dobra na variabe or na vlue dono ko ham chnge ni kr skhty.
- console.log(accountId,accountEmail,accountCity,accountpassword);
+//  console.log(accountId,accountEmail,accountCity,accountpassword);
  accountEmail="fizza786gmail.com"
  accountpassword="7869"
  accountCity="Lahore"
- console.table([accountId,accountEmail,accountpassword,accountCity,accountState]);
+//  console.table([accountId,accountEmail,accountpassword,accountCity,accountState]);
  /*Perfer not to use var.becasue of issue in block scope and functional scope.*/
 
+"use strick;"// treat all js code as newer version
+//alert("helo")// we are using nodejs,not browser coding k andr sb se zda chz jo h wo reuabilty h.
+// console.log(3+3); console.log("heloo")// asa ni krna javascript me its not h good code readabilty should be hight
+// javascript ki documentation jo h zda tar jo h na wo MDN he recommended ki jti h lkn ais ki basic jo documentation h wo na ecma script h aye h basci docmentation of javascript.
+// DataTypes java cript k nadr etny zda datatypes ni h.
+let name='Fizza'// ye hmere string datatype ha jo ham double qiute me lekhty h oasy ham string datatype khy gy.
+let age=18// ye hmere number datatype ha jis ham number wgra pas kty hn.
+let  loggedin=true// ye hmere bolean datatype ais me ham jo bi value true or false me dety hn wo hmere bolean datatype ho gi.
+// javscript me number datatype ki jo range h jo safe triky se javscript jo hndle kr leti h 2 to power 53 hai
+// ais k bigint bi ak datatype h ais ko ham basially etna use ni krty ais me ko ham tb use krty hn jb ham ne koi bht bra dena ho.
+ // nul bi ak datatype basically ye ak standalone value bi ha.
+ //undefined datatype h jis me ham varaible declare krty hn lkn ais ko value assign ni krty.
+ //symbol asi ko ham basically reat me zda use kty hn unquiness k lye .
+ // ye jo abi ham ne jin datatype ki bt ki h wo primitive hn primtive datat type me  string,number,null,undefined, bolean,symbol,bigint
+ // ab bt krty hn non -primitive datatype ki jis me sb se phly bt kre gy 
+ // object 
+ // array
+ // function
+ console.log(typeof "name"); // jb hm koi bi value double qoute me dy gy to oas k typeof jo h wo hmesha string me aye ga.
+ console.log( typeof age);
+ console.log( typeof undefined);
+ console.log( typeof null);
+ console.log(typeof Object)
+  console.log(typeof symbol)
+   console.log(typeof Array)
+   console.log(typeof function name(){});
 
+ 
